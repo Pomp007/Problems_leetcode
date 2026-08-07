a0f1c6129fe2761844d539abfa505a7ec376c20f
Pomp007 <166392953+Pomp007@users.noreply.github.com>
2026-08-07 13:53:28 +0530

    Time: 185 ms (14.42%), Space: 110.5 MB (32.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-
-        for (int num : nums) {
-            sum += num;
-        }
-
-        // Odd total cannot be divided equally
-        if (sum % 2 != 0)
-            return false;
-
-        int target = sum / 2;
-
-        // dp[j] = whether sum j can be formed
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-        for (int num : nums) {
-            // Traverse backwards so each number is used only once
-            for (int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
+    bool solve(int i ,vector<vector<int>>& dp , vector<int>& nums ,int target){
+        if(i == nums.size() ){
+            if(target == 0){
+                return true;
+            }
+            else{
+               return false;
             }
         }
+        if(dp[i][target] != -1){
+            return dp[i][target];
+        }
+        bool notake = solve(i + 1, dp, nums, target);
+        bool take = false;
+        if(target >= nums[i]){
+            take = solve(i + 1 , dp , nums, target - nums[i]);
+        }
 
-        return dp[target];
+        return dp[i][target] = (take || notake );
+
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum = sum + nums[i];
+        }
+        if(sum%2 != 0){
+            return false;
+        }
+        int target = sum/2;
+        vector<vector<int>> dp(n , vector<int>(target+1 , -1));
+        return solve(0 , dp , nums, target );
+        
     }
 };
