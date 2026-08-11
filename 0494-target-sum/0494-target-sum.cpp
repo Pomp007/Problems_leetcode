7578af9e2738a5166a068a13098168b42204891f
Pomp007 <166392953+Pomp007@users.noreply.github.com>
2026-08-11 21:19:35 +0530

    Time: 6 ms (68.71%), Space: 15.7 MB (37.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
 
-    int solve(vector<int>& nums, int n, int target, int sum) {
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
 
-        // all elements used
-        if (n == 0) {
-            if (sum == target) {
-                return 1;
-            }
-            else {
-                return 0;
+        if (abs(target) > sum) {
+            return 0;
+        }
+
+        if ((target + sum) % 2 != 0) {
+            return 0;
+        }
+
+        int maxSum = (target + sum) / 2;
+
+        vector<vector<int>> dp(n + 1,
+                               vector<int>(maxSum + 1, 0));
+
+        // Base case
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= maxSum; j++) {
+
+                // Don't take
+                int notake = dp[i - 1][j];
+
+                // Take
+                int take = 0;
+                if (nums[i - 1] <= j) {
+                    take = dp[i - 1][j - nums[i - 1]];
+                }
+
+                dp[i][j] = take + notake;
             }
         }
 
-        // put - before current number
-        int minus = solve(nums, n - 1, target,
-                          sum - nums[n - 1]);
-
-        // put + before current number
-        int plus = solve(nums, n - 1, target,
-                         sum + nums[n - 1]);
-
-        return minus + plus;
-    }
-
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-
-        int n = nums.size();
-        int sum = 0;
-
-        return solve(nums, n, target, sum);
+        return dp[n][maxSum];
     }
 };
